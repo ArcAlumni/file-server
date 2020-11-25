0104c3ddac678688b244f369916d7b760313caf6
Priyan <priyanpaulraj@gmail.com>
2020-11-25 21:27:20 +0530

    websocket client

--- src/main/java/com/fs/fs/config/WebSocketConfig.java
+++ src/main/java/com/fs/fs/config/WebSocketConfig.java
@@ -1,12 +1,14 @@
 package com.fs.fs.config;
 
-import com.fs.fs.handler.SimpleTextWebSocketHandler;
+import com.fs.fs.handler.WebSocketHandler;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -15,8 +17,16 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(new SimpleTextWebSocketHandler(), "/web-socket")
+        webSocketHandlerRegistry.addHandler(new WebSocketHandler(), "/web-socket")
         .setAllowedOrigins("*");
     }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxBinaryMessageBufferSize(1024000);
+        return container;
+    }
+    
 
 }
